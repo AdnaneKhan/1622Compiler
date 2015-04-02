76311f9757a9d7a44765392e44d3419f317f9666
Adnan <adnankhan@gmail.com>
2015-04-01 21:19:18 -0400

    types

--- src/IR/ThreeAddressStatement.java
+++ src/IR/ThreeAddressStatement.java
@@ -2,13 +2,23 @@ package IR;
 
 abstract class ThreeAddressStatement {
 
-
+    public final static int ASSIGNMENT = 1;
+    public final static int UNARY_ASSIGNMENT= 2;
+    public final static int COPY = 3;
+    public final static int UNCONDITIONAL_JUMP = 4;
+    public final static int CONDITIONAL_JUMP = 5;
+    public final static int PARAMETER = 6;
+    public final static int CALL = 7;
+    public final static int RETURN_3AC = 8;
+    public final static int INDEXED_ASSIGNMENT= 9;
+    public final static int NEW_3AC = 10;
+    public final static int NEW_ARRAY = 11;
+    public final static int LENGTH_3AC = 12;
 
     /**
      *
      * @return type of the statement (human readable, for debugging, etc)
      */
     public abstract String statementType();
-
 
 }
