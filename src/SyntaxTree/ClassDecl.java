008d1045b72f0afafa1bf19f59d0ac70aef6d49a
Adnan <adnankhan@gmail.com>
2015-04-02 08:15:45 -0400

    changes

--- src/SyntaxTree/ClassDecl.java
+++
@@ -1,8 +0,0 @@
-package SyntaxTree;
-import Visitor.Visitor;
-import Visitor.TypeVisitor;
-
-public abstract class ClassDecl extends ASTNode {
-  public abstract void accept(Visitor v);
-  public abstract Type accept(TypeVisitor v);
-}
--- src/SyntaxTree/Exp.java
+++
@@ -1,8 +0,0 @@
-package SyntaxTree;
-import Visitor.Visitor;
-import Visitor.TypeVisitor;
-
-public abstract class Exp extends ASTNode {
-  public abstract void accept(Visitor v);
-  public abstract Type accept(TypeVisitor v);
-}
