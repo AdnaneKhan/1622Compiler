af6dec55f7d1adad6d98d4518eb12f36a8dfc78d
Adnan <adnankhan@gmail.com>
2015-04-02 07:45:01 -0400

    added more checker methods for scopes

--- src/SymTable/TableEntry.java
+++ src/SymTable/TableEntry.java
@@ -1,6 +1,7 @@
 package SymTable;
 
 import SyntaxTree.ASTNode;
+import javafx.scene.control.Tab;
 
 import java.util.HashMap;
 
@@ -31,38 +32,116 @@ public abstract  class TableEntry {
     }
 
      protected  TableEntry(ASTNode actualNode) {
-         hash = new HashMap<KeyWrapper,TableEntry>();
+
+         if (!this.isEntry(LEAF_ENTRY)) {
+             hash= new HashMap<KeyWrapper,TableEntry>();
+         }
+
         this.actualNode = actualNode;
         this.lineNum = actualNode.lineNum();
         this.charNum = actualNode.charNum();
     }
 
+
+    /**
+     *
+     @param key string key to lookup, this is the identifier in the code
+      * @param entry_type Class, Method, or Type (leaf
+     *                   meaning int[], boolean, int or Object
+     * @return true if the type exists in THIS SCOPE
+     */
     public boolean hasEntry(String key, int entry_type) {
-        KeyWrapper checkWrap = new KeyWrapper(key,entry_type);
 
-        if (this.hash.containsKey(checkWrap)) {
-            return true;
-        } else
-        {
-            return false;
+        if (! this.isEntry(LEAF_ENTRY)) {
+            KeyWrapper checkWrap = new KeyWrapper(key,entry_type);
+
+            if (this.hash.containsKey(checkWrap)) {
+                return true;
+            }
         }
+      return false;
     }
 
+    /**
+     *
+     * @param key string key to lookup, this is the identifier in the code
+     * @param entry_type Class, Method, or Type (leaf
+     *                   meaning int[], boolean, int or Object
+     * @return null if entry does not exist with the specified type
+     */
     public TableEntry getEntry(String key, int entry_type) {
-        KeyWrapper checkWrap = new KeyWrapper(key,entry_type);
-
-        if (this.hash.containsKey(checkWrap)) {
-            return hash.get(checkWrap);
-        } else
-        {
-            return null;
+        if (this.entryType() != LEAF_ENTRY) {
+            KeyWrapper checkWrap = new KeyWrapper(key,entry_type);
+            if (this.hash.containsKey(checkWrap)) {
+                return hash.get(checkWrap);
+            }
         }
+        return null;
     }
 
+    /**
+     *
+     * @param key
+     * @param entry_type
+     * @return the entry if it exists in this scope OR any of its parent scopes
+     */
+    public TableEntry getEntryWalk(String key, int entry_type) {
+        TableEntry retV = null;
+
+        TableEntry cursor =this;
+
+        do {
+            if (this.entryType() != LEAF_ENTRY) {
+                retV = this.hash.get(new KeyWrapper(key,entry_type));
+                if (retV != null) {
+                    break;
+                }
+            }
+            cursor = cursor.parent;
+        } while (cursor != null);
+
+        return retV;
+    }
+
+    /**
+     *
+     * @param key
+     * @param entry_type
+     * @return true if the entry if it exists in this scope OR any of its parent scopes
+     */
+    public boolean hasEntryWalk(String key, int entry_type) {
+
+        boolean found = false;
+        TableEntry cursor =this;
+
+        do {
+
+           if (this.entryType() != LEAF_ENTRY) {
+                TableEntry val = this.hash.get(new KeyWrapper(key,entry_type));
+               if (val != null) {
+                   found = true;
+                   break;
+               }
+           }
+                cursor = cursor.parent;
+        } while (cursor != null);
+
+
+       return found;
+    }
+
+    /**
+     The string name of this symbol associated with the ID type
+     there may be more names in which case the getNode method must be used
+     */
     public String getSymbolName() {
         return symbolName;
     }
 
+    /**
+     *
+     * @return the actual AST node wrapped by this entry
+     */
     public ASTNode getNode() {
         return actualNode;
     }
@@ -77,7 +156,6 @@ public abstract  class TableEntry {
      *
      */
     public abstract int entryType();
-
 
     /**
      *
