db15f1e627be49967fe94b01e8002903d9c9cb59
Adnan <adnankhan@gmail.com>
2015-04-19 22:46:51 -0400

    ms 9

--- milestone_tests/milestone9.java
+++ milestone_tests/milestone9.java
@@ -11,9 +11,11 @@ class Foo {
 class Fac extends Foo {
     public int ComputeFac(int num){
         int[] i;
+        int[] theArray;
         int x;
         int y;
         int z;
+        ArrayCopy ac;
 
         i = new int [60];
         z = 4;
@@ -37,6 +39,60 @@ class Fac extends Foo {
         System.out.println(q);
         System.out.println(i[2]);
 
-        return z + q + i [2];
+
+        ac = new ArrayCopy();
+
+
+        z = ac.setK(99898989);
+
+        System.out.println(ac.getK());
+
+        theArray = ac.makeArray();
+
+        theArray = ac.populateArray(theArray);
+
+        return z + q;
     }
+
+}
+
+class ArrayCopy {
+    int b;
+    int c;
+    int k;
+
+    public int setK(int var) {
+        k = var;
+
+        return 0;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public int[] makeArray() {
+        int[] a;
+        a = new int[10];
+        return a;
+    }
+
+    public int[] populateArray(int[] toPopulate) {
+        int iter;
+
+        iter = 0;
+
+        while (iter < 10) {
+            toPopulate[iter] = iter;
+            System.out.println(iter);
+            iter = iter + 1;
+        }
+
+        return toPopulate;
+    }
+
+
+
+
+
 }
